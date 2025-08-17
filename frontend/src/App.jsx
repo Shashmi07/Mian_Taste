d79035ca487209cb4765b87e523477691d79bf86
Mithsala123 <piyumisudusinghe02@gmail.com>
2025-08-17 14:53:45 +0530

    create menu page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Menu from './Pages/menu/Ramen'
+import PreOrder from './Pages/PreOrder'
+import Rice from './Pages/menu/Rice'
+import Soup from './Pages/menu/Soup'
+import Drink from './Pages/menu/Drink'
+import More from './Pages/menu/More'
 
-import NavBar from './Components/NavBar.jsx'
+
 
 const App = () => {
   return (
-    <div className='app'>
-      <NavBar />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/PreOrder" element={<PreOrder />} />
+        <Route path="/rice" element={<Rice />} />
+        <Route path="/ramen" element={<Menu />} />
+        <Route path="/soup" element={<Soup />} />
+        <Route path="/drink" element={<Drink />} />
+        <Route path="/more" element={<More />} />
+    
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 import { User } from 'lucide-react';
 import logo from "../assets/logo.jpeg";
 import Search from "../assets/search.png";
@@ -11,28 +13,27 @@ const NavBar = () => {
   return (
     <nav 
       className="fixed top-0 w-full flex items-center h-20 z-50 shadow-md" 
-      style={{ backgroundColor: '#78D860' }}
+      style={{ backgroundColor: '#3ed853ff' }}
     >
       {/* Logo */}
       <div className="flex items-center ml-10">
         <img 
-          src={logo} // Use imported Logo variable
+          src={logo}
           alt="logo" 
           style={{ width: '60px', height: '60px' }}
-          className=" rounded-full cursor-pointer "
+          className="rounded-full cursor-pointer"
         />
       </div>
 
       {/* Menu Items */}
       <ul className="flex m-0 p-0 list-none font-semibold text-lg flex-1 justify-center" style={{ gap: '20px' }}>
-
         {menuItems.map((item) => (
           <li
             key={item}
             onClick={() => setMenu(item)}
             className={`cursor-pointer capitalize transition-all duration-300 ${
               menu === item 
-                ? 'pb-0.5 border-b-2 ' 
+                ? 'pb-0.5 border-b-2' 
                 : 'hover:text-blue-500 text-gray-700'
             }`}
             style={menu === item ? { 
@@ -40,11 +41,17 @@ const NavBar = () => {
               color: '#49557e' 
             } : {}}
           >
-            {item === 'pre-order'
-              ? 'Pre-Order'
-              : item === 'table reservation'
-              ? 'Table Reservation'
-              : item}
+            {item === 'menu' ? (
+              <Link to="/menu" className="text-inherit no-underline">
+                Menu
+              </Link>
+            ) : item === 'pre-order' ? (
+              'Pre-Order'
+            ) : item === 'table reservation' ? (
+              'Table Reservation'
+            ) : (
+              item
+            )}
           </li>
         ))}
       </ul>
@@ -53,7 +60,7 @@ const NavBar = () => {
       <div className="flex items-center gap-6 mr-10" style={{ transform: 'translateX(-20px)' }}>
         {/* Search Icon */}
         <img 
-          src={Search} // Use imported Search variable
+          src={Search}
           alt="Search" 
           style={{ width: '50px', height: '50px' }}
           className="cursor-pointer hover:opacity-70 transition-opacity duration-300"
@@ -62,13 +69,13 @@ const NavBar = () => {
         {/* Cart Icon */}
         <div className="relative cursor-pointer">
           <img 
-            src={Cart} // Use imported Cart variable
+            src={Cart}
             alt="Cart" 
             style={{ width: '40px', height: '40px' }}
-            
-            className="w-6 h-6 hover:opacity-70 transition-opacity duration-300"/>
+            className="w-6 h-6 hover:opacity-70 transition-opacity duration-300"
+          />
           {/* Notification Badge */}
-          <span className="absolute -top-1 -right-1 w-2 h-2 rounded-full border-2 border-white"style={{ backgroundColor: 'red' }} ></span>
+          <span className="absolute -top-1 -right-1 w-2 h-2 rounded-full border-2 border-white" style={{ backgroundColor: 'red' }}></span>
         </div>
 
         {/* Sign In Button */}
